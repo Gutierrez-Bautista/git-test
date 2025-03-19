40bfcc97f75abe60480c2a8d228d8cd6e6c3a73d
Bauti <bautistamgutierrez@gmail.com>
2025-03-19 20:35:45 -0300

    v3

--- 2.py
+++ 2.py
@@ -1,1 +1,11 @@
 print('aaa')
+
+'''
+  main ----------- v1 ----- v2 ------------------- v3 ------ merge
+    |---> login ----|        |----> comunicados ----|
+'''
+
+'''
+user: git config --global user.name <userName>
+email: git config --global user.email <email>
+'''
